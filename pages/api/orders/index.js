8be4f1f4325b23cece5dd93dd9f500679034390c
matt-plant-auth0 <matthew.plant@auth0.com>
2024-02-06 15:51:05 +0000

    fix orders api

--- pages/api/orders/index.js
+++ pages/api/orders/index.js
@@ -6,12 +6,13 @@ export default async function handler(req, res) {
         let orderId = ordersRepo.create(req.body.order);
         return res.status(200).json({ orderId: orderId });
     }else if(req.method === "GET"){
+        console.log(req.query);
         let orderSearchResults = ordersRepo.find(x => x.email === req.query.email);
         let orderIds = [];
         if(orderSearchResults && orderSearchResults.length > 0){
           orderIds = orderSearchResults.map(order => { return order.id });
         }
-        return res.status(200).json(orderIds);
+        return res.status(200).json({ orderIds: orderIds });
     }else{
         return res.status(400).json({ msg: "HTTP Method unsupported" });
     }
